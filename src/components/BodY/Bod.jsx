e4c772c775124dd9f8a1fb473d2ac5d958d47eb4
Selvakumar Boominathan <selvans2k@gmail.com>
2024-04-05 00:21:40 +0530

    layout responsiveness fixed

--- src/components/BodY/Bod.jsx
+++ src/components/BodY/Bod.jsx
@@ -12,112 +12,104 @@ const Bod = () => {
 
     <Container className="container-Bod">
       <Row>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Fancy Product</Card.Title>
+            <Card.Text>
+              $50.00 $25.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Fancy Product</Card.Title>
+            <Card.Text>
+              $120.00 - $280.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Special Item</Card.Title>
+            <Card.Text>
+              $100.00 - $220.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Special Item</Card.Title>
+            <Card.Text>
+              $18.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
       </Row>
       <Row>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Special Item</Card.Title>
+            <Card.Text>
+              $40.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Sale Item</Card.Title>
+            <Card.Text>
+              $130.00 - $220.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Sale Item</Card.Title>
+            <Card.Text>
+              $100.00 - $220.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
+        {/* <Col> */}
+        <Card style={{ width: '12rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Popular Item</Card.Title>
+            <Card.Text>
+              $190.00 - $220.00
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+        {/* </Col> */}
       </Row>
     </Container>
   )
